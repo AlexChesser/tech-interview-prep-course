b98abf0d2215dafda6fe91090469884cf83c685e
Alexander Chesser <alex.chesser@gmail.com>
2021-04-15 21:54:45 -0400

    first attempt - interested to see advanced test cases

--- string-to-int/test/StringToIntTests.cs
+++ string-to-int/test/StringToIntTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace test
@@ -7,10 +8,14 @@ namespace test
     {
         [TestMethod]
         [DataRow("42", 42)]
+        [DataRow("", 0)]
+        [DataRow("-42", -42)]
         [DataRow("   -42", -42)]
         [DataRow("4193 with words", 4193)]
         [DataRow("words and 987", 0)]
         [DataRow("-91283472332", -2147483648)]
+        [DataRow("-9128347233", -2147483648)] // 10 digit negative number that is out of bounds
+
         public void Should_return_expected_value(string input, int expected)
         {
             int actual = new Solution().MyAtoi(input);

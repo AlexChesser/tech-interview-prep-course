e2f29d43c15043d96e595ee41a448acaeb21f4f6
Alexander Chesser <alex.chesser@gmail.com>
2021-11-02 22:58:59 -0400

    Day Two problem set

--- challenges/lc/AlgorithmsCrashCourse/test/Tests.cs
+++ challenges/lc/AlgorithmsCrashCourse/test/Tests.cs
@@ -1,16 +1,23 @@
-using System;
 using System.Diagnostics;
 using System.Linq;
-using _278_FirstBadVersion;
-using _35_SearchInsertPosition;
-using _704_BinarySearch;
-using _977_SquaresofaSortedArray;
 using NUnit.Framework;
 
 namespace test
 {
     public class Tests
     {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDow()
+        {
+            Trace.Flush();
+        }
+
         [SetUp]
         public void Setup()
         {
@@ -57,18 +64,6 @@ namespace test
             Assert.AreEqual(result, expected, $"target {target} expected {expected}");
         }
 
-
-        /*
-        Input: nums = [-4,-1,0,3,10]
-        Output: [0,1,9,16,100]
-        Explanation: After squaring, the array becomes [16,1,0,9,100].
-        After sorting, it becomes [0,1,9,16,100].
-        Example 2:
-
-        Input: nums = [-7,-3,2,3,11]
-        Output: [4,9,9,49,121]
-        */
-
         [Test]
         [TestCase("-4,-1,0,3,10", "0,1,9,16,100")]
         [TestCase("-7,-3,2,3,11", "4,9,9,49,121")]
@@ -79,7 +74,20 @@ namespace test
                 .Select(i => int.Parse(i))
                 .ToArray();
             int[] result = s.SortedSquares(haystack);
-            Assert.AreEqual(string.Join(",", result), expected, $"records did not match");
+            Assert.AreEqual(expected, string.Join(",", result), $"records did not match");
+        }
+
+        [Test]
+        [TestCase("1,2,3,4,5,6,7", 3, "5,6,7,1,2,3,4")]
+        [TestCase("-1,-100,3,99", 2, "3,99,-1,-100")]
+        public void _189_RotateArray(string arr, int k, string expected)
+        {
+            var s = new _189_RotateArray.Solution();
+            int[] haystack = arr.Split(",")
+                .Select(i => int.Parse(i))
+                .ToArray();
+            s.Rotate(haystack, k);
+            Assert.AreEqual(expected, string.Join(",", haystack), $"shifting {arr} {k} times gives {expected} got { string.Join(",", haystack)}");
         }
 
     }

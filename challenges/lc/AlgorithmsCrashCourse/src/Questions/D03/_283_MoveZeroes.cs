b08c060defc35f2f0dc3fc2ed00ac56238a2ecd2
Alexander Chesser <alex.chesser@gmail.com>
2021-11-03 06:33:27 -0400

    solved 283

--- challenges/lc/AlgorithmsCrashCourse/src/Questions/D03/_283_MoveZeroes.cs
+++ challenges/lc/AlgorithmsCrashCourse/src/Questions/D03/_283_MoveZeroes.cs
@@ -5,10 +5,20 @@ namespace _283_MoveZeroes
 {
     public class Solution
     {
-
         public void MoveZeroes(int[] nums)
         {
-
+            int left = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[left++] = nums[i];
+                }
+            }
+            for (int i = left; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
         }
     }
 }

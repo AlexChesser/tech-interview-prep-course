6b54da96417ef7f31358396587977eede4e730bc
Alexander Chesser <alex.chesser@gmail.com>
2021-11-06 21:06:16 -0400

    wip house robber 1 & 2

--- challenges/LeetCode/src/LearningPaths/Algorithms/D12/_198_HouseRobber.cs
+++ challenges/LeetCode/src/LearningPaths/Algorithms/D12/_198_HouseRobber.cs
@@ -1,10 +1,33 @@
+using System;
+
 namespace _198_HouseRobber
 {
+    /*
+    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+    */
+
     public class Solution
     {
         public int Rob(int[] nums)
         {
-            return 0;
+            int n = nums.Length;
+            if (n == 0)
+            {
+                return 0;
+            }
+            int next, nextPlusOne;
+            nextPlusOne = 0;
+            next = nums[n - 1];
+
+            for (int i = n - 2; i >= 0; --i)
+            {
+                int current = Math.Max(next, nextPlusOne + nums[i]);
+                nextPlusOne = next;
+                next = current;
+            }
+            return next;
         }
     }
 }
--- challenges/LeetCode/src/LearningPaths/DynamicProgramming/_213_HouseRobberII.cs
+++ challenges/LeetCode/src/LearningPaths/DynamicProgramming/_213_HouseRobberII.cs
@@ -1,10 +1,37 @@
+using System;
+
 namespace  _213_HouseRobberII
 {
     public class Solution
     {
+        private int RangedRob(int[] nums, int start, int finish)
+        {
+            int n = nums.Length;
+            if (n == 0)
+            {
+                return 0;
+            }
+            int next, nextPlusOne;
+            nextPlusOne = 0;
+            next = nums[n - 1];
+
+            for (int i = start; i < finish; --i)
+            {
+                int current = Math.Max(next, nextPlusOne + nums[i]);
+                nextPlusOne = next;
+                next = current;
+            }
+            return next;
+        }
+
         public int Rob(int[] nums)
         {
-            return 0;
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+            if (nums.Length == 2) return Math.Max(nums[0], nums[1]);
+            int track1 = RangedRob(nums, 0, nums.Length - 2);
+            int track2 = RangedRob(nums, 1, nums.Length - 1);
+            return Math.Max(track1, track2);
         }
     }
 }

7ee541fd78cffa259616ba15963ea54f4ad17fe1
Alexander Chesser <alex.chesser@gmail.com>
2021-11-06 22:37:59 -0400

    uggh... still really trying to wrap my head around DP

--- challenges/LeetCode/src/LearningPaths/DynamicProgramming/_740_DeleteandEarn.cs
+++ challenges/LeetCode/src/LearningPaths/DynamicProgramming/_740_DeleteandEarn.cs
@@ -9,55 +9,41 @@ namespace  _740_DeleteandEarn
     {
         public int DeleteAndEarn(int[] nums)
         {
-            Dictionary<int, int> map = new Dictionary<int, int>();
-            for (int i = 0; i < nums.Length; i++)
+            int[] count = new int[10001];
+            foreach (int num in nums)
             {
-                if (!map.ContainsKey(nums[i]))
-                {
-                    map.Add(nums[i], nums[i]);
-                }
-                else
-                {
-                    map[nums[i]] += nums[i];
-                }
+                count[num]++;
             }
-            int max = 0;
+            int skip = 0;
+            int take = 0;
+            int prev = -1;
+            // start at zero but 
+            // increment the index before we use it.
+            // meaning we effectively start at one
+            for (int i = 0; i < count.Length; ++i)
+            {
+                // if the current index has values
+                if (count[i] > 0)
+                {
+                    // set the current value to the larger
+                    // of our previous iteration
+                    int m = Math.Max(skip, take);
+                    // if our previous number is within a range of 1
+                    if (i - 1 != prev)
+                    {
 
-            List<int> out1 = new List<int>();
-            List<int> out2 = new List<int>();
-            foreach (var key in map.Keys.OrderBy(k => k))
-            {
-                out1.Add(key);
-                out2.Add(map[key]);
-            }
-            Trace.WriteLine(out1.ToArray().CommaJoin());
-            Trace.WriteLine(out2.ToArray().CommaJoin());
-            HashSet<int> skip = new HashSet<int>();
-            foreach (int key in map.Keys.OrderBy(k => k))
-            {
-                if (skip.Contains(key))
-                {
-                    continue;
-                }
-                int current = map[key];
-                int next = map.ContainsKey(key + 1) ? map[key + 1] : 0;
-                int nextPlus = map.ContainsKey(key + 2) ? map[key + 2] : 0;
-                if (current + nextPlus > next)
-                {
-                    Trace.WriteLine($"skipping {key + 1} because {current + nextPlus} > {next}");
-                    skip.Add(key + 1);
-                    max += current;
-                }
-                else
-                {
-                    Trace.WriteLine($"taking {key + 1} because {current + nextPlus} > {next}");
-                    skip.Add(key);
-                    skip.Add(key + 1);
-                    skip.Add(key + 2);
-                    max += next;
+                        take = i * count[i] + m;
+                        skip = m;
+                    }
+                    else
+                    {
+                        take = i * count[i] + skip;
+                        skip = m;
+                    }
+                    prev = i;
                 }
             }
-            return max;
+            return Math.Max(skip, take);
         }
     }
 }

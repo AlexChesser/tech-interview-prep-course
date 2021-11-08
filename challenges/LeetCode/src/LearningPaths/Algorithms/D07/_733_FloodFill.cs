3bad0c3b82c9d2ecd994498101f5273619610859
Alexander Chesser <alex.chesser@gmail.com>
2021-11-07 19:38:47 -0500

    oops!

--- challenges/LeetCode/src/LearningPaths/Algorithms/D07/_733_FloodFill.cs
+++ challenges/LeetCode/src/LearningPaths/Algorithms/D07/_733_FloodFill.cs
@@ -7,7 +7,7 @@ namespace _733_FloodFill
         private void ChangeNeighbors(int[][] image, int row, int col, int from, int to)
         {
             //Console.WriteLine($"setting {image.ToNestedString()} r:{row} c:{col} f:{from} t:{to}");
-            if (image[row][col] == from) return;
+            if (image[row][col] != from) return;
             image[row][col] = to;
             if (row > 0) ChangeNeighbors(image, row - 1, col, from, to);
             if (col > 0) ChangeNeighbors(image, row, col - 1, from, to);
